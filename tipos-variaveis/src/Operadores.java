37c0f921a5c9e0aedfcb81b9f59d8d50cee7aae3
Leonardo Fiaes <leonardofiaes@hotmail.com>
2025-04-14 18:14:39 -0300

    meu projeto conta banco

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,47 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+       String concatenacao = "?"; 
+
+       concatenacao = 1+1+1+"1";
+
+       System.out.println(concatenacao);
+
+       concatenacao = 1+"1"+1+1;
+
+       System.out.println(concatenacao);
+
+       concatenacao = 1+"1"+1+"1";
+
+       System.out.println(concatenacao);
+
+       concatenacao = "1"+1+1+1;
+
+       System.out.println(concatenacao);
+
+       concatenacao = "1"+(1+1+1);
+
+       System.out.println(concatenacao);
+
+
+       //Operador ternário
+
+       /*int a, b;
+       a = 6;
+       b = 6;
+
+       String resultado = a==b ? "verdadeiro" : "false";
+
+       System.out.println(resultado);
+        */
+
+        //Operador relacionais
+
+        /*
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao =
+         */
+    }
+}
