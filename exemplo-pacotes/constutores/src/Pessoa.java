bff373f4740cb0699a0ca50830be12232136d3df
Leonardo Fiaes <leonardofiaes@hotmail.com>
2025-04-23 16:06:49 -0300

    Aprendendo sobre construtores

---
+++ exemplo-pacotes/constutores/src/Pessoa.java
@@ -0,0 +1,35 @@
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	public Pessoa (String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+    
+    public Pessoa(String nome) {
+        super();
+        this.nome = nome;
+    }
+
+    public Pessoa() {
+        super();
+    }
+
+    public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String newEndereco) {
+		endereco = newEndereco;
+	}
+	//...
+	//setters de nome e cpf ?
+}
+
