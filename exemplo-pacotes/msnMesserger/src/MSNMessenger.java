5805e0b20bb8c488a5c1d1a3550d7f7fbc46edbc
Leonardo Fiaes <leonardofiaes@hotmail.com>
2025-04-23 21:34:06 -0300

    Implementando o conceito de Herança

--- exemplo-pacotes/msnMesserger/src/MSNMessenger.java
+++ exemplo-pacotes/msnMesserger/src/MSNMessenger.java
@@ -1,18 +1,4 @@
 
-public class MSNMessenger {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem");
-    }
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    //+30
-    }
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public class MSNMessenger extends ServicoMensagemInstantanea {
+    
 }
