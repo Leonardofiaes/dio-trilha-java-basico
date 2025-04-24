be080681c8b5c8c2ffcffa44659b593c5e97620e
Leonardo Fiaes <leonardofiaes@hotmail.com>
2025-04-23 22:11:17 -0300

    Implementação dos conceitos de Polimorfismo

--- exemplo-pacotes/msnMesserger/src/ComputadorPedrinho.java
+++ exemplo-pacotes/msnMesserger/src/ComputadorPedrinho.java
@@ -1,21 +1,25 @@
+import apps.FacebookMessenger;
+import apps.MSNMessenger;
+import apps.ServicoMensagemInstantanea;
+import apps.Telegram;
 
 public class ComputadorPedrinho {
     public static void main(String[] args) {
-        System.out.println("MSN");
-        MSNMessenger msn = new MSNMessenger();
-        msn.enviarMensagem();
-        msn.receberMensagem();
-
-        System.out.println("FACEBOOK");
+        ServicoMensagemInstantanea smi = null;
+        /*
+            NÃO SE SABE QUAL APP
+            MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+        */
+        String appEscolhido = "msn";
         
-        FacebookMessenger fcb = new FacebookMessenger();
-        fcb.enviarMensagem();
-        fcb.receberMensagem();
+        if (appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if (appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if (appEscolhido.equals("tlg"))
+            smi = new Telegram();
 
-        System.out.println("TELEGRAM");
-
-        Telegram tlg = new Telegram();
-        tlg.enviarMensagem();
-        tlg.receberMensagem();
+            smi.enviarMensagem();
+            smi.receberMensagem();
     }
 }
